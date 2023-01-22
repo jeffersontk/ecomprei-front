5c0e4a92be9c94eb040ab3cdda54f43e0cee0a51
jefferson brito <jscabral@firjan.com.br>
2023-01-21 21:39:09 -0300

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -11,7 +11,12 @@ const nextConfig = {
         pathname: '/wk5c55kzi/**',
       },
     ],
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
