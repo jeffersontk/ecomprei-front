0086173a8e9501afc9a574daf5b5847b060a4e89
jefferson brito <jscabral@firjan.com.br>
2023-02-17 20:12:39 -0300

    change next url in next config

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
     NEXT_TRACKING_API_TOKEN:'820f460506c5da59fb52c9e626105dfe54c8b73b26f19882ae9d903b5ffb10c6',
     NEXT_SMTP_USER:"contato@ecomprei.shop",
     NEXT_SMTP_PASSWORD:"Diana08012023",
-    NEXT_URL:"http://localhost:3000"
+    NEXT_URL:"https://ecomprei.shop/"
   }
 }
 
